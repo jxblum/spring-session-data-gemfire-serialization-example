631120a048e2dd5651d3b8f0f326d66ef9638bcd
John Blum <jblum@pivotal.io>
2019-02-15 17:03:47 -0800

    Add Gfsh shell scripts to start a GemFire Cluster manually.
    Add GemFire cache.xml file to configure and initialize GemFire for Spring Session.

---
+++ native-gemfire-server/src/main/resources/geode/bin/start-cluster.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#gfsh -e "run --file=${SYS_USER_HOME}/pivdev/spring-data-examples-workspace/spring-session-data-gemfire-serialization-example/native-gemfire-server/src/main/resources/geode/bin/start-cluster.gfsh"
+
+gfsh -e "run --file=/Users/jblum/pivdev/spring-data-examples-workspace/spring-session-data-gemfire-serialization-example/native-gemfire-server/src/main/resources/geode/bin/start-cluster.gfsh"
