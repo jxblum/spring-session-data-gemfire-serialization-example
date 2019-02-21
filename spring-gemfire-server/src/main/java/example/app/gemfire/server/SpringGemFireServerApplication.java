54e9ceb7972316bf03678a6b7753692b2b23131b
John Blum <jblum@pivotal.io>
2019-02-20 20:24:07 -0800

    Add BeanPostProcessor to disable Spring Session Data GemFire Indexes.

--- spring-gemfire-server/src/main/java/example/app/gemfire/server/SpringGemFireServerApplication.java
+++ spring-gemfire-server/src/main/java/example/app/gemfire/server/SpringGemFireServerApplication.java
@@ -1,13 +1,23 @@
 package example.app.gemfire.server;
 
+import static org.springframework.data.gemfire.util.CollectionUtils.asSet;
+
+import java.util.Set;
+
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.server.CacheServer;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
 import org.springframework.data.gemfire.config.annotation.EnableLocator;
 import org.springframework.data.gemfire.config.annotation.EnableManager;
+import org.springframework.lang.Nullable;
 import org.springframework.session.data.gemfire.config.annotation.web.http.EnableGemFireHttpSession;
 import org.springframework.session.data.gemfire.config.annotation.web.http.GemFireHttpSessionConfiguration;
 
@@ -39,6 +49,7 @@ import org.springframework.session.data.gemfire.config.annotation.web.http.GemFi
 )
 @EnableLocator
 @EnableManager(start = true)
+@SuppressWarnings("unused")
 public class SpringGemFireServerApplication {
 
   public static void main(String[] args) {
@@ -47,5 +58,42 @@ public class SpringGemFireServerApplication {
       .web(WebApplicationType.NONE)
       .build()
       .run(args);
+  }
+
+  @Bean
+  @Profile("disable-session-indexes")
+  BeanPostProcessor sessionIndexDisablingBeanPostProcessor() {
+
+    Set<String> sessionIndexBeanNames = asSet("principalNameIndex", "sessionAttributesIndex");
+
+    return new BeanPostProcessor() {
+
+      @Nullable @Override @SuppressWarnings("all")
+      public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return isIndexBean(bean, beanName) ? DummyObjectFactoryBean.INSTANCE : bean;
+      }
+
+      private boolean isIndexBean(Object bean, String beanName) {
+        return sessionIndexBeanNames.contains(beanName);
+      }
+    };
+  }
+
+  @SuppressWarnings("all")
+  private static final class DummyObjectFactoryBean implements FactoryBean<Object> {
+
+    private static final DummyObjectFactoryBean INSTANCE = new DummyObjectFactoryBean();
+
+    private static final Object DUMMY = new Object();
+
+    @Nullable @Override
+    public Object getObject() throws Exception {
+      return DUMMY;
+    }
+
+    @Nullable @Override
+    public Class<?> getObjectType() {
+      return DUMMY.getClass();
+    }
   }
 }
