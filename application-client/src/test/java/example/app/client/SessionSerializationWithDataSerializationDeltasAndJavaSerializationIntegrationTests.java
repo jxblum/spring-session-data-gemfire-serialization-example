8e8adf82690f62f70f28ed04af12df82287732c0
John Blum <jblum@pivotal.io>
2019-02-25 14:45:30 -0800

    Modify sessionWithCustomerSerializationWorks test case to create and save a new Session, touch the Session, add a Session Attribute and then update the Session Attribute in individual data access operations/transactions in order to better control the de/serialization process and delta propagation.

--- application-client/src/test/java/example/app/client/SessionSerializationWithDataSerializationDeltasAndJavaSerializationIntegrationTests.java
+++ application-client/src/test/java/example/app/client/SessionSerializationWithDataSerializationDeltasAndJavaSerializationIntegrationTests.java
@@ -3,6 +3,7 @@ package example.app.client;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.data.gemfire.util.ArrayUtils.nullSafeArray;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -44,6 +45,13 @@ public class SessionSerializationWithDataSerializationDeltasAndJavaSerialization
 
   private static final String GEMFIRE_LOG_LEVEL = "error";
 
+  private static Session touch(Session session) {
+
+    session.setLastAccessedTime(Instant.now());
+
+    return session;
+  }
+
   @Autowired
   private ClientCache clientCache;
 
@@ -55,17 +63,17 @@ public class SessionSerializationWithDataSerializationDeltasAndJavaSerialization
 
     assertThat(this.clientCache).isNotNull();
     assertThat(this.clientCache.getPdxSerializer()).isNull();
-    assertThat(isCustomerSerializerRegistered()).isFalse();
+    assertThat(isCustomerDataSerializerRegistered()).isFalse();
   }
 
-  private boolean isCustomerSerializerRegistered() {
+  private boolean isCustomerDataSerializerRegistered() {
 
     return Arrays.stream(nullSafeArray(InternalDataSerializer.getSerializers(), DataSerializer.class))
       .filter(Objects::nonNull)
-      .anyMatch(isCustomerSerializer());
+      .anyMatch(isCustomerDataSerializer());
   }
 
-  private Predicate<DataSerializer> isCustomerSerializer() {
+  private Predicate<DataSerializer> isCustomerDataSerializer() {
 
     return dataSerializer ->
       Arrays.stream(nullSafeArray(dataSerializer.getSupportedClasses(), Class.class))
@@ -76,12 +84,63 @@ public class SessionSerializationWithDataSerializationDeltasAndJavaSerialization
   @Test
   public void sessionWithCustomerSerializationWorks() {
 
+    // 1. Create and Save (new) Session
     Session session = this.sessionRepository.createSession();
 
     assertThat(session).isNotNull();
     assertThat(session.getId()).isNotEmpty();
     assertThat(session.isExpired()).isFalse();
     assertThat(session.getAttributeNames()).isEmpty();
+
+    this.sessionRepository.save(session);
+
+    // 2. Update (touch) Session, sending delta causing the Session to be deserialized on the server
+    session = touch(session);
+
+    this.sessionRepository.save(session);
+
+    // 3. Add Session Attribute
+    Customer jonDoe = addCustomerJonDoe(session);
+
+    this.sessionRepository.save(session);
+
+    //Customer janeDoe = addCustomerJaneDoe(session);
+
+    // 4. Update Session Attribute, sending delta causing the Session Attribute (Customer; Jon Doe)
+    // to be deserialized on the server
+    jonDoe = updateCustomer(session, "jonDoe", jonDoe);
+
+    this.sessionRepository.save(session);
+
+    Session loadedSession = this.sessionRepository.findById(session.getId());
+
+    assertThat(loadedSession).isEqualTo(session);
+    assertThat(loadedSession).isNotSameAs(session);
+    assertThat(loadedSession.getAttributeNames()).containsOnly("jonDoe");
+    assertThat(loadedSession.<Customer>getAttribute("jonDoe")).isEqualTo(jonDoe);
+
+    /*
+    assertThat(loadedSession.getAttributeNames()).containsOnly("jonDoe", "janeDoe");
+    assertThat(loadedSession.<Customer>getAttribute("jonDoe")).isEqualTo(jonDoe);
+    assertThat(loadedSession.<Customer>getAttribute("janeDoe")).isEqualTo(janeDoe);
+    */
+  }
+
+  private Customer addCustomerJaneDoe(Session session) {
+
+    Customer janeDoe = Customer.newCustomer("Jane Doe")
+      .asFemale()
+      .using(Phone.newPhone("503-555-9876"));
+
+    session.setAttribute("janeDoe", janeDoe);
+
+    assertThat(session.getAttributeNames()).contains("janeDoe");
+    assertThat(session.<Customer>getAttribute("janeDoe")).isEqualTo(janeDoe);
+
+    return janeDoe;
+  }
+
+  private Customer addCustomerJonDoe(Session session) {
 
     Customer jonDoe = Customer.newCustomer("Jon Doe")
       .asMale()
@@ -92,27 +151,14 @@ public class SessionSerializationWithDataSerializationDeltasAndJavaSerialization
     assertThat(session.getAttributeNames()).containsOnly("jonDoe");
     assertThat(session.<Customer>getAttribute("jonDoe")).isEqualTo(jonDoe);
 
-    this.sessionRepository.save(session);
+    return jonDoe;
+  }
 
-    Customer janeDoe = Customer.newCustomer("Jane Doe")
-      .asFemale()
-      .using(Phone.newPhone("503-555-9876"));
+  private Customer updateCustomer(Session session, String sessionAttributeName, Customer customer) {
 
-    session.setAttribute("janeDoe", janeDoe);
+    session.setAttribute(sessionAttributeName, customer.using(Phone.newPhone("503-555-5151")));
 
-    assertThat(session.getAttributeNames()).containsOnly("jonDoe", "janeDoe");
-    assertThat(session.<Customer>getAttribute("jonDoe")).isEqualTo(jonDoe);
-    assertThat(session.<Customer>getAttribute("janeDoe")).isEqualTo(janeDoe);
-
-    this.sessionRepository.save(session);
-
-    Session loadedSession = this.sessionRepository.findById(session.getId());
-
-    assertThat(loadedSession).isEqualTo(session);
-    assertThat(loadedSession).isNotSameAs(session);
-    assertThat(loadedSession.getAttributeNames()).containsOnly("jonDoe", "janeDoe");
-    assertThat(loadedSession.<Customer>getAttribute("jonDoe")).isEqualTo(jonDoe);
-    assertThat(loadedSession.<Customer>getAttribute("janeDoe")).isEqualTo(janeDoe);
+    return customer;
   }
 
   @ClientCacheApplication(logLevel = GEMFIRE_LOG_LEVEL, subscriptionEnabled = true)
